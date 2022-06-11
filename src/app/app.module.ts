3e6b94479577bdc542444874df7c4237584ae44b
David Nin <dninmur@gmail.com>
2022-06-11 20:10:40 +0200

    creating a good structure of files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ImageSliderComponent } from './image-slider/image-slider.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { WelcomeSelectionComponent } from './welcome-selection/welcome-selection.component';
-import { FooterComponent } from './footer/footer.component';
-import { WidgetConnectComponent } from './widget-connect/widget-connect.component';
-import { RandomNftComponent } from './random-nft/random-nft.component';
-import { ComponentNftComponent } from './component-nft/component-nft.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ImageSliderComponent } from './components/image-slider/image-slider.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { WelcomeSelectionComponent } from './components/welcome-selection/welcome-selection.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WidgetConnectComponent } from './components/widget-connect/widget-connect.component';
+import { RandomNftComponent } from './components/random-nft/random-nft.component';
+import { ComponentNftComponent } from './components/component-nft/component-nft.component';
 
 @NgModule({
   declarations: [
